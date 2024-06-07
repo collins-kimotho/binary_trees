c85efc9925d54e5c475a29f6a1922a77dc01f85b
collins-kimotho <collinskimotho16@gmail.com>
2024-06-07 10:56:18 +0000

    binary tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,13 +10,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t lh = 0, rh = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	lh = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	rh = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return ((lh > rh ? lh : rh));
 }
